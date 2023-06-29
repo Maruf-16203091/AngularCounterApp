ce2faf0dfd7d0b99f9f12015013f7709d5f89686
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2023-06-29 11:41:24 +0200

    Completed App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counterapp';
+
+  countNumber = 0;
+
+  //resetnumber
+  resetBtn() {
+    this.countNumber = 0;
+  }
+
+  //increasenumber
+  increaseBtn() {
+    this.countNumber = this.countNumber + 1;
+  }
+
+  //decreasenumber
+  decreaseBtn() {
+    this.countNumber = this.countNumber - 1;
+    if (this.countNumber < 0) {
+      this.countNumber = 0;
+    }
+  }
+
 }
